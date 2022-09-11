d2385f50cba0d35356e6e392faeffef1efeac563
Vladislav <wlladisla@gmail.com>
2022-09-11 19:56:52 +0300

    feat: add components

---
+++ src/components/Login.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div className='authContainer'>
+
+      Autorization
+      <p>login</p>
+      <p>password</p>
+
+      </div>
+  )
+}
+
+export default Login
