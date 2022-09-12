50e4c67a156cbde4d655eb91a31b1c15e5670e7c
Vladislav <wlladisla@gmail.com>
2022-09-12 18:01:00 +0300

    feat: add styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+
+import {Login} from './components/Login'
 
 function App() {
   return (
-    <h1>canalService</h1>
+    <Login/>
   );
 }
 
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,15 +1,33 @@
-import React from 'react'
+import styled, { css }  from 'styled-components'
 
-function Login() {
+interface IBtn {
+  primary: boolean,
+
+}
+
+const Button = styled.button<IBtn>`
+background: transparent;
+border-radius: 6px;
+border: 2px solid;
+color: palevioletred;
+margin: 0.5em 1em;
+padding: 0.25em 1em;
+
+${props => props.primary && css`
+  background: #E4B062;
+  color: white;
+`}
+`;
+
+ export function Login() {
   return (
     <div className='authContainer'>
 
-      Autorization
+      <h1>Autorization</h1>
       <p>login</p>
       <p>password</p>
-
+      <Button primary>Submit</Button>
       </div>
   )
 }
 
-export default Login
